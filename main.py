a6578c687c0da8ceafc9ed8dbf879a385ab9212c
Vlad <lupu6999@gmail.com>
2026-09-20 17:10:22 +0200

    Add lenght check for numbers

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 print("Welcome to my console calculator!\n")
 
-def read_numbers(number):
+def read_numbers(prompt):
     while True:
         try:
-            return float(input(number))
+           number = int(input(prompt))
         except ValueError:
             print("Please enter a number!")
+            continue
+        text = str(number)
+        if len(text) <= 6:
+            return int(text)
+        print("Too many numbers!")
+
+
 a = read_numbers("Enter A: ")
 b = read_numbers("Enter B: ")
 
